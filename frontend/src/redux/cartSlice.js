1177ab5f538eda0ad7b24bbf8ceed66a8f738821
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2024-09-03 04:22:34 +0530

    Update cartSlice.js

--- frontend/src/redux/cartSlice.js
+++ frontend/src/redux/cartSlice.js
@@ -9,16 +9,7 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addToCart: (state, action) => {
-      state.items.push(action.payload);
-    },
-    removeFromCart: (state, action) => {
-      const { id, size } = action.payload;
-      const index = state.items.findIndex(item => item.id === id && item.size === size);
-      if (index !== -1) {
-        state.items.splice(index, 1);
-      }
-    },
+   
     moveToOrders: (state) => {
       state.orders.push(...state.items); // Move items from cart to orders
       state.items = []; // Clear items from cart
