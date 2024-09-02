56a3964bde7d5e6d7df959e9d3488979c6648011
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2024-09-03 04:12:53 +0530

    Update blog.js

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,7 +1,3 @@
-import express from 'express';
-const router=express.Router();
-import fetchUser from '../middleware/fetchUser.js';  //middleware
-import Blog from '../models/Blog.js';  //notes model
 
 //route 1
 router.post("/addblog",fetchUser,async (req,res)=>{
